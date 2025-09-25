9da6cc004663965fb78ec24aa8fc387dec6a642d
Shreya Pandey <shreya.p@nighthack.in>
2025-09-26 01:20:10 +0530

    removed name

--- src/schemas.py
+++ src/schemas.py
@@ -2,12 +2,10 @@ from pydantic import BaseModel, EmailStr
 
 # Subscriber schemas
 class SubscriberCreate(BaseModel):
-    name: str
     email: EmailStr
 
 class SubscriberResponse(BaseModel):
     id: int
-    name: str
     email: EmailStr
     is_active: bool
 
